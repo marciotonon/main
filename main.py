e16bd06cddc6162aaba970a45ae8379f5acd412c
marciotonon <marciotonon@gmail.com>
2023-12-11 22:23:41 -0300

    fix

--- main.py
+++ main.py
@@ -16,9 +16,29 @@ class Usuario(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     senha = db.Column(db.String(120), nullable=False)
 
+class Categoria(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80), nullable=False)
+    descricao = db.Column(db.String(255), unique=True, nullable=False)
+
+    # Establishing a one-to-many relationship with Produto
+    produtos = db.relationship('Produto', backref='categoria', lazy=True)
+
+class Produto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80), nullable=False)
+    preco = db.Column(db.Float, nullable=False)
+
+    # Establishing a foreign key relationship with Categoria
+    categoria_id = db.Column(db.Integer, db.ForeignKey('categoria.id'), nullable=False)
+
 def create_app():
     # Verifica se o banco de dados não existe antes de criar todas as tabelas
     if not os.path.exists('usuarios.db'):
+        with app.app_context():
+            db.create_all()
+
+    if not os.path.exists('categoria.db'):
         with app.app_context():
             db.create_all()
 
@@ -59,6 +79,48 @@ def login():
             return render_template('home.html', usuario=usuario)
 
     return render_template('login.html')
+
+#rotas produtos
+@app.route('/produtos')
+def produtos():
+    produtos = Produto.query.all()
+    return render_template('produtos.html', produtos=produtos)
+
+@app.route('/new_produto')
+def new_produto():
+    return render_template('cadastrar_produto.html')
+
+@app.route('/cadastrar_produto', methods=['POST'])
+def cadastrar_produto():
+    nome = request.form['nome']
+    preco = request.form['preco']
+
+    novo_produto = Produto(nome=nome, preco=preco)
+    db.session.add(novo_produto)
+    db.session.commit()
+
+    return redirect(url_for('produtos'))
+
+#rotas categoria
+@app.route('/listacategoria')
+def categoria():
+    categorias = Produto.query.all()
+    return render_template('listaCategoria.html', categorias=categorias)
+
+@app.route('/new_categoria')
+def new_categoria():
+    return render_template('cadastrar_categoria.html')
+
+@app.route('/cadastrar_categoria', methods=['POST'])
+def cadastrar_categoria():
+    nome = request.form['nome']
+    descricao = request.form['preco']
+
+    novo_categoria = Categoria(nome=nome, descricao=descricao)
+    db.session.add(novo_categoria)
+    db.session.commit()
+
+    return redirect(url_for('categorias'))
 
 if __name__ == '__main__':
     create_app().run(debug=True)
