70aed9d42cdfba0dcb368503fdca0b424408178c
marciotonon <marciotonon@gmail.com>
2023-12-13 11:15:01 -0300

    fix

--- classes.py
+++ classes.py
@@ -11,13 +11,22 @@ class Usuario(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     senha = db.Column(db.String(100), nullable=False)
 
+    # Relacionamento um-para-muitos com Categoria
+    categorias = db.relationship('Categoria', backref='usuario', lazy=True)
+
+    # Relacionamento um-para-muitos com Produto
+    produtos = db.relationship('Produto', backref='usuario', lazy=True)
+
 
 class Categoria(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(80), nullable=False)
     descricao = db.Column(db.String(255), nullable=False)
 
-    # Estabelecendo uma relação um-para-muitos com Produto
+    # Relacionamento muitos-para-um com Usuario
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+
+    # Relacionamento um-para-muitos com Produto
     produtos = db.relationship('Produto', backref='categoria', lazy=True)
 
 class Produto(db.Model):
@@ -25,6 +34,8 @@ class Produto(db.Model):
     nome = db.Column(db.String(80), nullable=False)
     preco = db.Column(db.Float, nullable=False)
 
-    # Relacionamento com Categoria
+    # Relacionamento muitos-para-um com Usuario
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+
+    # Relacionamento muitos-para-um com Categoria
     categoria_id = db.Column(db.Integer, db.ForeignKey('categoria.id'), nullable=False)
-    
--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import flash
 from classes import db, Usuario, Categoria, Produto
-from flask_paginate import Pagination, get_page_args
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager
+from rotas import rotas_app
 
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usuarios.db'
+app.register_blueprint(rotas_app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usuarios1.db'
 app.config['SECRET_KEY'] = 'DeuséFieloTempoTodo'
 db.init_app(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-# Configurar para servir arquivos estáticos (CSS, JS, etc.)
-app.static_folder = 'static'
-app.template_folder = 'templates'
+login_manager.login_view = 'rotas_app.login'
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -41,179 +38,7 @@ def create_app():
 
 PRODUTOS_POR_PAGINA = 4
 
-
-@app.route('/new')
-def new():
-    usuarios = Usuario.query.all()
-    return render_template('cadastrar.html', usuarios=usuarios)
-
-
-@app.route('/cadastrar', methods=['POST'])
-def cadastrar():
-    nome = request.form['nome']
-    email = request.form['email']
-    senha = request.form['senha']
-
-    novo_usuario = Usuario(nome=nome, email=email, senha=senha)
-    db.session.add(novo_usuario)
-    db.session.commit()
-
-    return redirect(url_for('index'))
-
-@app.route('/')
-def index():
-    usuarios = Usuario.query.all()
-    return render_template('login.html', usuarios=usuarios)
-
-# Rota dashboard (protegida por login)
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('home.html', usuario=current_user)
-
-
-# Rota de login
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        senha = request.form['senha']
-        usuario = Usuario.query.filter_by(email=email, senha=senha).first()
-
-        if usuario:
-            login_user(usuario)
-            flash('Login realizado com sucesso.', 'success')
-            return redirect(url_for('dashboard'))
-        else:
-            flash('Credenciais inválidas. Tente novamente.', 'error')
-
-    return render_template('login.html')
-
-# Rota de logout
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('Logout realizado com sucesso.', 'success')
-    return redirect(url_for('index'))
-
-#rotas produtos
-@app.route('/listaprodutos')
-@login_required
-def produtos():
-    # Configurando a paginação
-    page = request.args.get('page', 1, type=int)
-    per_page = 10  # Defina o número desejado de itens por página
-
-    # Obtendo a lista de produtos paginada
-    produtos_paginados = Produto.query.paginate(page=page, per_page=per_page, error_out=False)
-
-    return render_template('listaprodutos.html', produtos_paginados=produtos_paginados)
-
-@app.route('/new_produto')
-@login_required
-def new_produto():
-    categorias = Categoria.query.all()
-    return render_template('cadastrar_produto.html', categorias=categorias)
-
-# Adicione esta rota para excluir uma categoria
-@app.route('/delete_produto/<int:produto_id>', methods=['GET', 'POST'])
-@login_required
-def delete_produto(produto_id):
-    # Obtém a categoria pelo ID
-    produto = Produto.query.get(produto_id)
-
-    # Verifica se a categoria existe
-    if produto:
-        # Remove a categoria do banco de dados
-        db.session.delete(produto)
-        db.session.commit()
-
-        flash('Produto excluída com sucesso.', 'success')  # Mensagem de sucesso
-    else:
-        flash('Produto não encontrada.', 'error')  # Mensagem de erro
-
-    return redirect(url_for('produtos'))
-
-@app.route('/cadastrar_produto', methods=['POST'])
-@login_required
-def cadastrar_produto():
-    nome = request.form['nome']
-    preco = request.form['preco']
-    categoria_id = request.form['categoria']  # Obtém a categoria selecionada
-
-    # Obtém a categoria pelo ID
-    categoria = Categoria.query.get(categoria_id)
-
-    if categoria:
-        novo_produto = Produto(nome=nome, preco=preco, categoria=categoria)
-        db.session.add(novo_produto)
-        db.session.commit()
-
-        flash('Produto cadastrado com sucesso.', 'success')
-    else:
-        flash('Categoria não encontrada.', 'error')
-
-    return redirect(url_for('produtos'))
-
-
-# Adicione esta rota para excluir uma categoria
-@app.route('/delete_categoria/<int:categoria_id>', methods=['GET', 'POST'])
-@login_required
-def delete_categoria(categoria_id):
-    # Obtém a categoria pelo ID
-    categoria = Categoria.query.get(categoria_id)
-
-    # Verifica se a categoria existe
-    if categoria:
-        # Remove a categoria do banco de dados
-        db.session.delete(categoria)
-        db.session.commit()
-
-        flash('Categoria excluída com sucesso.', 'success')  # Mensagem de sucesso
-    else:
-        flash('Categoria não encontrada.', 'error')  # Mensagem de erro
-
-    return redirect(url_for('categorias'))
-
-
-# Suas rotas categoria existentes
-@app.route('/listacategoria')
-@login_required
-def categorias():
-    categorias = Categoria.query.all()
-    return render_template('listaCategoria.html', categorias=categorias)
-
-@app.route('/new_categoria')
-@login_required
-def new_categoria():
-    return render_template('cadastrar_categoria.html')
-
-@app.route('/cadastrar_categoria', methods=['GET', 'POST'])
-@login_required
-def cadastrar_categoria():
-    if request.method == 'POST':
-        nome = request.form['nome']
-        descricao = request.form['descricao']
-
-        # Verificar se a categoria já existe pelo nome e descrição
-        categoria_existente = Categoria.query.filter_by(nome=nome, descricao=descricao).first()
-
-        if categoria_existente:
-            flash('Categoria já existe.', 'error')  # Mensagem de erro
-            return render_template('cadastrar_categoria.html')
-
-        # Se não existir, criar a nova categoria
-        novo_categoria = Categoria(nome=nome, descricao=descricao)
-        db.session.add(novo_categoria)
-        db.session.commit()
-
-        flash('Categoria cadastrada com sucesso.', 'success')  # Mensagem de sucesso
-
-        return redirect(url_for('categorias'))
-
-    # Tratar o método GET aqui (se necessário)
-    return render_template('cadastrar_categoria.html')
-
 if __name__ == '__main__':
-    create_app().run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
